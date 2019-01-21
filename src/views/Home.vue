6c1603d771581ba304f96b2a0523f170f93564da
hurk <1072529589@qq.com>
2019-01-21 10:48:34 +0800

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    <h1>This a Vue demo</h1>
+    <!--
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+//import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+  // name: 'home',
+  // components: {
+  //   HelloWorld
+  // }
 }
 </script>
