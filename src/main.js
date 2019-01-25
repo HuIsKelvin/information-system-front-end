0bef99770251c65c675c8885de7b8ba7c84f597d
hurk <1072529589@qq.com>
2019-01-25 14:17:29 +0800

    #

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
+
+// 将axios写入vue的原型
+Vue.prototype.axios = axios
 
 Vue.config.productionTip = false
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -21,7 +21,7 @@ export default {
     // HelloWorld,
     // coma,
     // comb
-  }
+  },
 }
 </script>
 
